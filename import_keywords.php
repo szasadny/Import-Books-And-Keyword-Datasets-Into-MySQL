2cec1328e3ee79fa8a99e0f0938911609c242cc8
szasadny <szasadny@live.nl>
2023-06-29 13:33:32 +0200

    duplicate key fix + added batch handling

--- import_keywords.php
+++ import_keywords.php
@@ -9,6 +9,7 @@ $table_book = "book";
 $table_book_keywords = "keyword_books";
 $table_keyword = "keyword";
 $csvFile = "books_keywords.csv";
+$batchSize = 5000;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -19,12 +20,14 @@ if ($conn->connect_error) {
 // Process CSV file
 if (($handle = fopen($csvFile, "r")) !== false) {
 
-    // Keep track of the duplicates locally to be able to send everything in one batch
-    $keywordsToAdd = array();
+    // Define counter for batch processing size
+    $counter = 0;
+
+    // Keep track of the existing keywords
     $keywordsExisting = array();
 
     // Prepare the INSERT statement for keywords
-    $insertKeywordStmt = $conn->prepare("INSERT INTO $table_keyword (keyword) VALUES (?)");
+    $insertKeywordStmt = $conn->prepare("INSERT IGNORE INTO $table_keyword (keyword) VALUES (?)");
     $insertKeywordStmt->bind_param("s", $keyword);
 
     // Prepare the INSERT statement for book-keyword associations
@@ -33,8 +36,8 @@ if (($handle = fopen($csvFile, "r")) !== false) {
 
     // Start a transaction
     $conn->begin_transaction();
-    
-    // Retrieve existing keyword and their id from the database
+
+    // Retrieve existing keywords from the database
     $sql = "SELECT id, keyword FROM $table_keyword";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
@@ -49,36 +52,50 @@ if (($handle = fopen($csvFile, "r")) !== false) {
     while (($data = fgetcsv($handle)) !== false) {
         $isbn = $data[0];
         $rawKeywords = explode(",", $data[1]);
-        
+
         // Normalize raw keywords
         $rawKeywords = array_map("trim", $rawKeywords);
-        
-        // Add all the keywords that need to be added
-        $keywordsToAdd = array_merge($keywordsToAdd, array_diff($rawKeywords, $keywordsExisting));
-    }
 
-    // Link each keyword to it's corresponding book.
-    $sql = "SELECT id FROM $table_book WHERE isbn = '$isbn'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $bookId = $result->fetch_assoc()["id"];
-
-        foreach ($keywordsToAdd as $keyword) {
-            $keyword = $conn->real_escape_string($keyword);
-            $insertKeywordStmt->execute();
-            $keywordId = $insertKeywordStmt->insert_id;
-            $keywordsExisting[$keyword] = $keywordId;
-            unset($keywordsToAdd[$keyword]);
-
-            $insertBookKeywordStmt->execute();
-            if ($insertBookKeywordStmt->errno) {
-                echo "Error linking keyword to book: " . $insertBookKeywordStmt->error;
+        foreach ($rawKeywords as $keyword) {
+            // If keyword already exists, use the existing id
+            if (isset($keywordsExisting[$keyword])) {
+                $keywordId = $keywordsExisting[$keyword];
+            // Else insert the new keyword and get its id
+            } else {
+                $insertKeywordStmt->execute();
+                $keywordId = $insertKeywordStmt->insert_id;
+                $keywordsExisting[$keyword] = $keywordId;
             }
+
+            // Link the keyword to the book
+            $sql = "SELECT id FROM $table_book WHERE isbn = '$isbn'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                $bookId = $result->fetch_assoc()["id"];
+
+                $insertBookKeywordStmt->execute();
+                if ($insertBookKeywordStmt->errno) {
+                    echo "Error linking keyword to book: " . $insertBookKeywordStmt->error;
+                }
+            }
+        }
+
+        $counter++;
+
+        // Execute batch insert after reaching the batch size
+        if ($counter === $batchSize) {
+            $conn->commit();
+
+            // Start a new transaction
+            $conn->begin_transaction();
+
+            // Reset the counter
+            $counter = 0;
         }
     }
 
-    // Commit the transaction
+    // Commit the remaining records
     $conn->commit();
 
     fclose($handle);
